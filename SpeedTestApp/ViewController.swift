199464533e16965a1999398617aaefad6b65ac17
Varya Utkina <varya2412@mail.ru>
2024-04-04 21:38:43 +0300

    Initial Commit

---
+++ SpeedTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpeedTestApp
+//
+//  Created by Варвара Уткина on 04.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
