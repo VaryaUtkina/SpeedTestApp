0f9c7e7f63654912dfb2f2e498289dbc9108e638
Varya Utkina <varya2412@mail.ru>
2024-04-08 23:40:56 +0300

    Work with logic of SettingsVC

--- SpeedTestApp/ViewControllers/SettingsViewController.swift
+++ SpeedTestApp/ViewControllers/SettingsViewController.swift
@@ -7,11 +7,100 @@
 
 import UIKit
 
+enum Alert {
+    case checkboxIsEmpty
+    case urlIsEmpty
+    case invalidURL
+    
+    var title: String {
+        switch self {
+        case .checkboxIsEmpty:
+            "What do you want to measure?"
+        case .urlIsEmpty:
+            "URL is empty"
+        case .invalidURL:
+            "Wrong URL!"
+        }
+    }
+    
+    var message: String {
+        switch self {
+        case .checkboxIsEmpty:
+            "Please choose type of speed"
+        case .urlIsEmpty:
+            "Please enter URL"
+        case .invalidURL:
+            "Please change URL"
+        }
+    }
+}
+
 final class SettingsViewController: UIViewController {
 
-    weak var delegate: SettingsViewControllerDelegate?
+    @IBOutlet var downloadCheckbox: UIButton!
+    @IBOutlet var uploadCheckbox: UIButton!
     
+    var downloadIsSelected: Bool!
+    var uploadIsSelected: Bool!
+    
+    weak var delegate: SettingsViewControllerDelegate?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupCheckbox(downloadIsSelected, for: downloadCheckbox)
+        setupCheckbox(uploadIsSelected, for: uploadCheckbox)
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            title: "Save",
+            style: .plain,
+            target: self,
+            action: #selector(backButtonTapped)
+        )
+    }
+    
+    @objc func backButtonTapped() {
+        if !downloadIsSelected && !uploadIsSelected {
+            showAlert(withStatus: .checkboxIsEmpty)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let speedVC = segue.destination as? SpeedViewController else { return }
+        speedVC.shouldMeasureDownloadSpeed = downloadIsSelected
+        speedVC.shouldMeasureUploadSpeed = uploadIsSelected
+    }
+    
+    @IBAction func downloadCheckboxTapped() {
+        downloadIsSelected.toggle()
+        setupCheckbox(downloadIsSelected, for: downloadCheckbox)
+        delegate?.didChangeDownloadSelection(downloadIsSelected)
+    }
+    
+    @IBAction func uploadCheckboxTapped() {
+        uploadIsSelected.toggle()
+        setupCheckbox(uploadIsSelected, for: uploadCheckbox)
+        delegate?.didChangeUploadSelection(uploadIsSelected)
+    }
+    
+    private func setupCheckbox(_ boolValue: Bool, for sender: UIButton) {
+        if boolValue {
+            sender.setImage(UIImage(systemName: "checkmark.rectangle.portrait"), for: .normal)
+        } else {
+            sender.setImage(UIImage(systemName: "rectangle.portrait"), for: .normal)
+        }
+    }
+    
+    private func showAlert(withStatus status: Alert) {
+        let alert = UIAlertController(
+            title: status.title,
+            message: status.message,
+            preferredStyle: .alert
+        )
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
     }
 }
--- SpeedTestApp/ViewControllers/SpeedViewController.swift
+++ SpeedTestApp/ViewControllers/SpeedViewController.swift
@@ -8,24 +8,37 @@
 import UIKit
 
 protocol SettingsViewControllerDelegate: AnyObject {
-    func setupURL()
+    func didChangeDownloadSelection(_ value: Bool)
+    func didChangeUploadSelection(_ value: Bool)
 }
 
 final class SpeedViewController: UIViewController {
+    
     @IBOutlet var downloadLabel: UILabel!
     @IBOutlet var currentDownloadSpeedLabel: UILabel!
     @IBOutlet var uploadLabel: UILabel!
     @IBOutlet var currentUploadLabel: UILabel!
     
+    @IBOutlet var downloadStack: UIStackView!
+    @IBOutlet var uploadStack: UIStackView!
+    
     let networkManager = NetworkManager.shared
     let serverURL = "https://www.speedtest.net"
+    var shouldMeasureDownloadSpeed = true
+    var shouldMeasureUploadSpeed = true
+    
     private var isTestingInProgress = false
-    private var shouldMeasureDownloadSpeed = true
-    private var shouldMeasureUploadSpeed = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingsVC = segue.destination as? SettingsViewController else { return }
+        settingsVC.delegate = self
+        settingsVC.downloadIsSelected = shouldMeasureDownloadSpeed
+        settingsVC.uploadIsSelected = shouldMeasureUploadSpeed
     }
     
     @IBAction func goButtonTapped(_ sender: RoundButton) {
@@ -57,12 +70,28 @@ final class SpeedViewController: UIViewController {
             }
         }
     }
+    
+    // ПОВТОРЕНИЕ!!! В двух VC одно и то же
+    private func showAlert(withStatus status: Alert) {
+        let alert = UIAlertController(
+            title: status.title,
+            message: status.message,
+            preferredStyle: .alert
+        )
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
 
 extension SpeedViewController: SettingsViewControllerDelegate {
-    func setupURL() {
-        
+    func didChangeDownloadSelection(_ value: Bool) {
+        shouldMeasureDownloadSpeed = value
+        downloadStack.isHidden = !value
     }
     
-    
+    func didChangeUploadSelection(_ value: Bool) {
+        shouldMeasureUploadSpeed = value
+        uploadStack.isHidden = !value
+    }
 }
